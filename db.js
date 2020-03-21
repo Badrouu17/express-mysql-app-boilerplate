42668ff916aa94dc7efe7be515cddcdb11194eac
Badrouu17 <badrouu.laabed@gmail.com>
2020-03-21 09:04:20 +0100

    modifiy env vars

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'admin',
-  database: 'batn-db'
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB
 });
 
 db.connect(err => {
